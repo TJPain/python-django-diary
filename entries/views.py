ac40d6f2260d3de42e3ffc4ca3c7592548f98206
Tom Pain <hello@tompa.in>
2021-11-02 18:41:32 +0000

    Flash messages and navigation buttons
    type: feat
    
    Flash messages for create, update and delete. Add, edit and delete buttons

--- entries/views.py
+++ entries/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
 from django.views.generic import (
     ListView,
@@ -7,31 +8,40 @@ from django.views.generic import (
     UpdateView,
     DeleteView,
 )
+
 from .models import Entry
+
 
 class EntryListView(ListView):
     model = Entry
-    #sort in ascending order
     queryset = Entry.objects.all().order_by("-date_created")
+
 
 class EntryDetailView(DetailView):
     model = Entry
 
-class EntryCreateView(CreateView):
+
+class EntryCreateView(SuccessMessageMixin, CreateView):
     model = Entry
     fields = ["title", "content"]
     success_url = reverse_lazy("entry-list")
+    success_message = "Your new entry was created!"
 
-class EntryUpdateView(UpdateView):
+
+class EntryUpdateView(SuccessMessageMixin, UpdateView):
     model = Entry
     fields = ["title", "content"]
+    success_message = "Your entry was updated!"
 
     def get_success_url(self):
-        return reverse_lazy(
-            "entry-detail",
-            kwargs={"pk": self.entry.id}
-        )
+        return reverse_lazy("entry-detail", kwargs={"pk": self.object.pk})
+
 
 class EntryDeleteView(DeleteView):
     model = Entry
     success_url = reverse_lazy("entry-list")
+    success_message = "Your entry was deleted!"
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(self.request, self.success_message)
+        return super().delete(request, *args, **kwargs)
